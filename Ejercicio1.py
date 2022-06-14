18da96ea00658eb57e3adc481df2f44f51c426fe
mxlgrrr <106144699+mxlgrrr@users.noreply.github.com>
2022-06-14 18:18:26 +0200

    Ejercicios Open Bootcamp Python

---
+++ Ejercicio1.py
@@ -0,0 +1,14 @@
+"""
+    Desde la consola de python, debes de imprimir las siguientes cadenas de texto:
+
+    Hola, soy [tu_nombre]
+
+    Estoy empezando el curso de python
+
+    Espero aprender mucho
+
+    Tienes que subir capturas de pantalla en una carpeta comprimida zip.
+"""
+print("Hola, soy Maximiliano")
+print("Estoy empezando el curso de python")
+print("Espero aprender mucho")
