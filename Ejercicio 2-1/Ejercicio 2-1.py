0f9c56e9af9890289b5e0b5d26cb4112086d9576
mxlgrrr <106144699+mxlgrrr@users.noreply.github.com>
2022-06-14 18:44:22 +0200

    Create Ejercicio 2-1.py

---
+++ Ejercicio 2-1/Ejercicio 2-1.py
@@ -0,0 +1,5 @@
+"""
+    En este ejercicio tendrás que crear una variable a la cual asignarás una cadena de texto y la imprimirás por consola, a su vez, tendrás que imprimir también el tipo de la variable por consola.
+    Este ejercicio lo tienes que realizar desde la consola de python y subir capturas de pantalla en una carpeta comprimida zip.
+"""
+
