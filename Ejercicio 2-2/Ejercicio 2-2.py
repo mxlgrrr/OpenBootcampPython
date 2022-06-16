d53fb1d86728f6262fcd84b0e91e6bda0d325cd1
mxlgrrr <106144699+mxlgrrr@users.noreply.github.com>
2022-06-16 10:46:31 +0200

    Ejercicio 2-2

---
+++ Ejercicio 2-2/Ejercicio 2-2.py
@@ -0,0 +1,12 @@
+"""
+    En este ejercicio tendrás que crear una variable a la cual asignarás una cadena de texto y la imprimirás por consola, a su vez, tendrás que imprimir también el tipo de la variable por consola.
+    Este ejercicio lo tienes que realizar desde la consola de python y subir capturas de pantalla en una carpeta comprimida zip.
+"""
+
+cadena = "Cadena de texto"
+print(cadena)
+print("El tipo de variable es %s", type(cadena))
+cadena = 244
+print(cadena)
+print("El tipo de variable es %s", type(cadena))
+
