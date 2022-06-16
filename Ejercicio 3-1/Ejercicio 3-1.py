01b03a89114f8074e207c469b8c114e4497f6add
mxlgrrr <106144699+mxlgrrr@users.noreply.github.com>
2022-06-17 00:29:12 +0200

    Ejercicio 3-1

---
+++ Ejercicio 3-1/Ejercicio 3-1.py
@@ -0,0 +1,44 @@
+"""
+    Para este ejercicio tenéis que crear en la consola de python variables que representen los siguientes datos de un contacto:
+
+    Nombre
+
+    Apellidos
+
+    Edad
+
+    Email
+
+    Teléfono
+
+    Casado (verdadero o falso)
+
+    Con Hijos (verdadero o falso)
+
+    Lista de amigos
+
+    Películas vistas (diccionario con clave y valor. El valor será el título de la película)
+
+    Una vez hayas creado todas las variables, muéstralas por consola haciendo uso de la función print().
+
+    Tienes que subir capturas de pantalla en una carpeta comprimida zip.
+"""
+nombre = "Maximiliano"
+apellidos = "Lazarte Guerrero"
+edad = 32
+email = "maximilianolazarteg@gmail.com"
+telefono = "608 54 16 15"
+casado = False
+conHijos = False
+listaDeAmigos = ["Jessica Fernandez","Vladimir Tokarev", "Cristian Gutierrez", "Ramon Puga"]
+peliculasVistas = {"Pelicula 1":"Iron Man","Pelicula 2":"Avengers","Pelicula 3":"Terminator","Pelicula 4":"Robocop"}
+
+print("Nombre: %s",nombre)
+print("Apellidos: %s",apellidos)
+print("Edad: %s",edad)
+print("Email: %s",email)
+print("Telefono: %s",telefono)
+print("Casado: %s",casado)
+print("Con Hijos: %s",conHijos)
+print("Lista de Amigos: %s",listaDeAmigos)
+print("Peliculas vistas: %s",peliculasVistas)
