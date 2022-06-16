56be347d8b45034f6fd90c118eb30f9154506863
mxlgrrr <106144699+mxlgrrr@users.noreply.github.com>
2022-06-17 01:23:37 +0200

    Ejercicio 4-1

---
+++ Ejercicio 4-1/Ejercicio 4-1.py
@@ -0,0 +1,8 @@
+"""
+    Escribe un programa que pregunte al usuario su edad y muestre por pantalla si es mayor de edad o no.
+"""
+edad = input("Ingrese su edad: ")
+if int(edad)>=18:
+    print("Usted es mayor de edad")
+else:
+    print("Usted es menor de edad")
