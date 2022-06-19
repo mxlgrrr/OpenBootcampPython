7291117a3b6f31c04fd1435b3ad8b22f756f885b
mxlgrrr <106144699+mxlgrrr@users.noreply.github.com>
2022-06-19 23:16:10 +0200

    Ejercicio 7-1

---
+++ Ejercicio 7-1/Ejercicio 7-1.py
@@ -0,0 +1,13 @@
+"""
+    En este ejercicio tendréis que crear un módulo que contenga las operaciones básicas de una calculadora: sumar, restar, multiplicar y dividir.
+    Este módulo lo importaréis a un archivo python y llamaréis a las funciones creadas. Los resultados tenéis que mostrarlos por consola.
+"""
+import operaciones
+
+numero_a = 3
+numero_b = 5
+
+print(operaciones.sumar(numero_a,numero_b))
+print(operaciones.restar(numero_a,numero_b))
+print(operaciones.multiplicar(numero_a,numero_b))
+print(operaciones.dividir(numero_a,numero_b))
---
+++ Ejercicio 7-1/operaciones.py
@@ -0,0 +1,16 @@
+
+def sumar(a,b):
+    resultado = a+b
+    return resultado
+
+def restar(a,b):
+    resultado = a-b
+    return resultado
+
+def multiplicar(a,b):
+    resultado = a*b
+    return resultado
+
+def dividir(a,b):
+    resultado = a/b
+    return resultado
